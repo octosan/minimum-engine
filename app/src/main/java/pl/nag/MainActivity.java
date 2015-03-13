c6fa7bbafd122964bcb660fd3aa6ea1b86d44f01
hexmind <hexmind@hexmind.pl>
2015-03-13 20:44:12 +0100

    clean

--- app/src/main/java/pl/nag/MainActivity.java
+++ app/src/main/java/pl/nag/MainActivity.java
@@ -1,10 +1,9 @@
-package com.nag.nag;
+package pl.nag;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 
 public class MainActivity extends Activity {
 
@@ -13,7 +12,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
