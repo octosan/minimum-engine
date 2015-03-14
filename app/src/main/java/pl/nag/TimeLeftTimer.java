ca4a4c701bf80495f4a084d060beafa56ddbdf78
mrvoid <wrong.void@gmail.com>
2015-03-14 13:05:06 +0000

    Timer ticktac

--- app/src/main/java/pl/nag/TimeLeftTimer.java
+++ app/src/main/java/pl/nag/TimeLeftTimer.java
@@ -10,8 +10,8 @@ import android.widget.ProgressBar;
 public class TimeLeftTimer extends CountDownTimer {
 
     private static final String TAG = TimeLeftTimer.class.getSimpleName();
-    private static final int MAX_SECONDS = 5;
-    private static final int COUNT_DOWN_INTERVAL_MS = 20;
+    private static final int MAX_SECONDS = 6;
+    private static final int COUNT_DOWN_INTERVAL_MS = 1000;
     private static final int ONE_SECOND_MS = 1000;
 
     private ProgressBar progressBar;
